a47485fce1da1735955eb254aeb0e8b27c32f4e5
Sushanthsp <sushanthspoojary777@gmail.com>
2023-04-13 07:56:37 +0530

    fixed bug

--- user/router/comments.route.js
+++ user/router/comments.route.js
@@ -5,13 +5,13 @@ const commentsValidation = require("../validator/comments.validator");
 const { verifyJWTToken } = require("../../middleware/jwt.middleware");
 
 router.post("/comments", commentsValidation.postComment,verifyJWTToken, commentsController.postComment);
-router.put("/comments", commentsValidation.updateComment, commentsController.updateComment);
+router.put("/comments", commentsValidation.updateComment, verifyJWTToken, commentsController.updateComment);
 router.delete("/comments/:id",commentsValidation.deleteComment, verifyJWTToken, commentsController.deleteComment);
 router.get("/comments", commentsValidation.getComment, commentsController.getComment);
 
 
-router.put("/reploy/:id", commentsValidation.updateComment, commentsController.postReply);
-router.put("/reploy/:id/:replyId",commentsValidation.deleteComment, verifyJWTToken, commentsController.deleteComment);
-router.get("/comments/:replyId", commentsValidation.getComment, commentsController.getComment);
+router.put("/reploy/:id", commentsValidation.postReplyValidation, verifyJWTToken, commentsController.postReply);
+router.put("/reploy/:id/:replyId",commentsValidation.updateReplyValidation, verifyJWTToken, commentsController.updateComment);
+router.delete("/reploy/:id/:replyId", verifyJWTToken, commentsController.deleteReply);
 
 module.exports = router;
